f2e32a1d0a1e17d9d6f8e37bebc4a009819c27e5
Jesse Hagewood <jesse.hagewood@gmail.com>
2012-10-07 13:20:33 -0400

    .

--- 2.lisp
+++ 2.lisp
@@ -5,9 +5,11 @@
 (defun fib(n)
 	(if (or (= n 0) (= n 1))
 		1
-		(+ (fib (- N 1)) (fib (- N 2)))))
+		(+ (fib (- n 1)) (fib (- n 2)))))
 
 (defun sumfib(n)
-	(if (= 0 (mod 2 (fib n)))
-		(+ n (sumfib (- n 1)))
-		(sumfib (- n 1))))
+	(if (= n 1)
+		1
+		(if (= 0 (mod 2 (fib n)))
+			(+ n (sumfib (- n 1)))
+			(sumfib (- n 1)))))
